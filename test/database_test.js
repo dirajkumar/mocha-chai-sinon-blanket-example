125e3d257164ee8cdfe6c925a8bd2238a7aa3764
dirajkumar <dirajkumar@gmail.com>
2014-08-05 13:23:18 -0400

    Updated the Read Me

--- test/database_test.js
+++ test/database_test.js
@@ -23,30 +23,30 @@ describe('Database Connection Tests using Spies', function() {
     });
 });
 
-// describe('Database Connection Tests using Stubs', function() {
-//     var db;
-//     var callback;
-//     beforeEach(function(){
-//         db = new Database();
-//         callback = sinon.spy();
-//     });
-//     it('Sucessfull Connection', function() {
-//         var stub = sinon.stub(db, 'init', function(username, password, url){
-//             db.emit('onSuccess', 'Connection Success from a Stub');
-//         });
-//         db.on('onSuccess', callback);
-//         db.init('diraj','pass','google.com');
-//         expect(callback.calledOnce).to.be.ok;
-//         expect(callback.getCall(0).args[0]).to.equal('Connection Success from a Stub');
-//     });
-//     it('Unsucessfull Connection', function() {
-//         var stub = sinon.stub(db, 'init', function(username, password, url){
-//             db.emit('onFailure', 'Connection Failed from a Stub');
-//         });
-//         db.on('onFailure', callback);
-//         db.init('','','google.com');
-//         expect(callback.calledOnce).to.be.ok;
-//         expect(callback.getCall(0).args[0]).to.equal('Connection Failed from a Stub');
-//     });
-// });
+describe('Database Connection Tests using Stubs', function() {
+    var db;
+    var callback;
+    beforeEach(function(){
+        db = new Database();
+        callback = sinon.spy();
+    });
+    it('Sucessfull Connection', function() {
+        var stub = sinon.stub(db, 'init', function(username, password, url){
+            db.emit('onSuccess', 'Connection Success from a Stub');
+        });
+        db.on('onSuccess', callback);
+        db.init('diraj','pass','google.com');
+        expect(callback.calledOnce).to.be.ok;
+        expect(callback.getCall(0).args[0]).to.equal('Connection Success from a Stub');
+    });
+    it('Unsucessfull Connection', function() {
+        var stub = sinon.stub(db, 'init', function(username, password, url){
+            db.emit('onFailure', 'Connection Failed from a Stub');
+        });
+        db.on('onFailure', callback);
+        db.init('','','google.com');
+        expect(callback.calledOnce).to.be.ok;
+        expect(callback.getCall(0).args[0]).to.equal('Connection Failed from a Stub');
+    });
+});
 
